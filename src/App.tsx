4307c5b8cc11f89c6acc48f5f54179eaa71c1749
ggsno <ogs0518@gmail.com>
2023-01-13 23:56:40 +0900

    feat: 메시지 송수신 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+if (typeof process.env.REACT_APP_WEB_SOCKET_BACKEND_URL !== "string") {
+  throw new Error("no environment value : WEB_SOCKET_BACKEND_URL");
+}
+
+const socket = io(process.env.REACT_APP_WEB_SOCKET_BACKEND_URL);
+
 function App() {
-  return <div></div>;
+  const [messages, setMessages] = useState<string[]>([]);
+  const [typingMessage, setTypingMessage] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (typingMessage) {
+      socket.emit("SEND_MESSAGE", { content: typingMessage });
+      setTypingMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("RECEIVE_MESSAGE", (receiveMessage: { content: string }) => {
+      setMessages(messages.concat(receiveMessage.content));
+    });
+    return () => {
+      socket.off("RECEIVE_MESSAGE");
+    };
+  }, [messages]);
+
+  return (
+    <>
+      {messages.map((message, index) => (
+        <div key={index}>{message}</div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          value={typingMessage}
+          onChange={(e) => setTypingMessage(e.target.value)}
+        />
+        <button>submit</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
